4066ec51426455d8fc08b691644de9d51202ddb9
Amar Ćatović <amar.catovic@wbee.io>
2021-03-14 12:23:07 +0100

    Add project files.

---
+++ RabbitMQ.Consumer/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Text;
+using System.Text.Json.Serialization;
+using Newtonsoft.Json;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+
+namespace RabbitMQ.Consumer
+{
+    static class Program
+    {
+        static void Main(string[] args)
+        {
+            var factory = new ConnectionFactory()
+            {
+                Uri = new Uri("amqp://guest:guest@localhost:5672")
+            };
+
+            using var connection = factory.CreateConnection();
+            using var channel = connection.CreateModel();
+            channel.QueueDeclare("amar01", true, false, false, null);
+
+            var consumer = new EventingBasicConsumer(channel);
+            consumer.Received += (sender, e) =>
+            {
+                var body = e.Body.ToArray();
+                var message = Encoding.UTF8.GetString(body);
+                Console.WriteLine($"{message}");
+            };
+
+            channel.BasicConsume("amar01", true, consumer);
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ RabbitMQ.Producer/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Text;
+using Newtonsoft.Json;
+using RabbitMQ.Client;
+
+namespace RabbitMQ.Producer
+{
+    static class Program
+    {
+        static void Main(string[] args)
+        {
+            var factory = new ConnectionFactory()
+            {
+                Uri = new Uri("amqp://guest:guest@localhost:5672")
+            };
+
+            using var connection = factory.CreateConnection();
+            using var channel = connection.CreateModel();
+            channel.QueueDeclare("amar01", true, false, false, null);
+            var message = new { Name = "Amar", Message = "RabbitMQ" };
+            var body = Encoding.UTF8.GetBytes(JsonConvert.SerializeObject(message));
+
+            channel.BasicPublish("", "amar01", null, body);
+        }
+    }
+}
